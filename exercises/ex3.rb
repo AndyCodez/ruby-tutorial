70d3a5ae92a318bd9a6df82f57c3354fa0f8fc3b
AndyCodez <andystudy4@gmail.com>
2016-06-23 09:44:20 +0300

    Rewrite to use floating point numbers

--- exercises/ex3.rb
+++ exercises/ex3.rb
@@ -1,7 +1,7 @@
 puts "I will now count my chickens:"
 
-puts "Hens #{25 +30 / 6}" #I'm expecting 30
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Hens #{25 +30 / 6.0}" #I'm expecting 30
+puts "Roosters #{100 - 25 * 3 % 4.0}"
 
 puts "Now I will count the eggs:"
 
@@ -11,8 +11,8 @@ puts "Is it true that 3 + 2 < 5 - 7?"
 
 puts 3 + 2 < 5 - 7
 
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 3 + 2? #{3 + 2.0}"
+puts "What is 5 - 7? #{5 - 7.0}"
 
 puts "Oh, that's why it's false."
 
