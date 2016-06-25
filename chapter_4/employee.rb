bd2d99eb0a85497a3547aab9523a83f048609e4f
AndyCodez <andystudy4@gmail.com>
2016-06-25 13:57:52 +0300

    Redefine initialize in the subclasses

--- chapter_4/employee.rb
+++ chapter_4/employee.rb
@@ -1,5 +1,9 @@
 class Employee
 attr_reader :name
+	
+	def initialize(name = "Anonymous")
+		self.name = name
+	end
 
 	def name=(name)
 		if name == ""
@@ -17,6 +21,10 @@ end
 class SalariedEmployee < Employee
 	attr_reader :salary
 
+	def initialize(name="Anonymous", salary = 0.0)
+		super(name)
+		self.salary = salary
+	end
 	def salary=(salary)
 		if salary < 0
 			raise "A salary of #{salary} aint valid"
@@ -31,4 +39,35 @@ class SalariedEmployee < Employee
 		puts "Pay this period: $#{formatted_pay}"
 	end
 
+end
+
+class HourlyEmployee < Employee
+	attr_reader :hourly_wage, :hours_per_week
+
+	def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+		super(name)
+		self.hourly_wage = hourly_wage
+		self.hours_per_week = hours_per_week
+	end
+
+	def hourly_wage=(hourly_wage)
+		if hourly_wage < 0
+			raise "An hourly wage of #{hourly_wage} isn't valid"
+		end
+		@hourly_wage = hourly_wage
+	end
+
+	def hours_per_week=(hours_per_week)
+		if hours_per_week < 0
+			raise "#{hours_per_week} hours per week is not valid"
+		end
+		@hours_per_week = hours_per_week
+	end
+
+	def print_pay_stub
+		print_name
+		pay_for_period = hourly_wage * hours_per_week * 2
+		formatted_pay = format("%.2f", pay_for_period)
+		puts "Pay this period: $#{formatted_pay}"
+	end
 end
