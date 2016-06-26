30e22080ad434032c715efcf76eb2f6e2d1891cc
AndyCodez <andystudy4@gmail.com>
2016-06-26 14:08:00 +0300

    Define methos to find adjectives in the reviews

--- chapter_6/reviews.rb
+++ chapter_6/reviews.rb
@@ -7,7 +7,15 @@ puts lines
 
 relevant_lines = lines.find_all { |line| line.include?("Truncated") }
 
-reviews = relevant_lines.reject { |line| line.include?("--") }
+reviews = relevant_lines.reject { |line| line.include?("--") }.to_s
 
-puts "Reviews BELOW:"
-puts reviews
+def find_adjective(string)
+  words = string.split(" ")
+  index = words.find_index("is")
+  words[index + 1]
+end
+
+puts "adjective BELOW:"
+adjective = find_adjective(reviews)
+puts adjective
+
