4c21f165d5db145d3d07acd97b58453a1551edae
AndyCodez <andystudy4@gmail.com>
2016-06-26 13:48:34 +0300

    Find relevant array elements using 'each'

--- chapter_6/reviews.rb
+++ chapter_6/reviews.rb
@@ -4,3 +4,14 @@ lines = review_file.readlines
 end
 
 puts lines
+
+relevant_lines = []
+
+lines.each do |line|
+  if line.include?("Truncated")
+    relevant_lines << line
+  end
+end
+
+puts "Relevant lines BELOW:"
+puts relevant_lines
